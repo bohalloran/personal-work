0c69972ecfdb359e8003df7782b5b2c42d0ca0ed
Brian O'Halloran <bohalloran>
2015-11-29 17:53:11 -0500

    add more tests

--- test_matrix_cross_product.py
+++ test_matrix_cross_product.py
@@ -23,5 +23,25 @@ class TestMatrixCrossProduct(unittest.TestCase):
         self.assertEqual(testMatrix[0][2], 3)
         self.assertEqual(testMatrix[2][2], 9)
 
+    def testAssymetricCorners(self):
+        numRows = 5
+        numCols = 3
+        matrix = MatrixCrossProduct(numRows,numCols)
+        testMatrix = matrix.loadMatrix()
+        self.assertEqual(testMatrix[0][0], 1)
+        self.assertEqual(testMatrix[numRows-1][0], 5)
+        self.assertEqual(testMatrix[0][numCols-1], 3)
+        self.assertEqual(testMatrix[numRows-1][numCols-1], 15)
+
+    def testLargerMatrixCorners(self):
+        numRows = 100
+        numCols = 100
+        matrix = MatrixCrossProduct(numRows,numCols)
+        testMatrix = matrix.loadMatrix()
+        self.assertEqual(testMatrix[0][0], 1)
+        self.assertEqual(testMatrix[numRows-1][0], 100)
+        self.assertEqual(testMatrix[0][numCols-1], 100)
+        self.assertEqual(testMatrix[numRows-1][numCols-1], 10000)
+
 if __name__ == "__main__":
     unittest.main()
