4eb9fff3401b40ac7354550fc4ffb23ac137c999
bohalloran <bohalloran@bitsighttech.com>
2016-03-08 17:17:52 -0500

    run equivalent of bs-feature

--- lib/commands.py
+++ lib/commands.py
@@ -4,43 +4,15 @@ import constants
 
 class Commands(object):
 
-    def __init__(self, pid):
-
-        self.pid = pid
+    def __init__(self):
 
         # Setup our fabric environment
         env.host_string = constants.SYSTEM_UNDER_TEST
         env.timeout = constants.REQUEST_TIMOUT
 
         # define our command set
-        self.numFileHandlesCmd = constants.LSOF_CMD + self.pid + '|' + constants.WC_CMD
-        self.meminfoCmd = constants.MEMINFO_CMD
-        self.systemLoadCmd = constants.SYSTEM_LOAD_CMD
-        self.lsofCmd = constants.LSOF_CMD
-        self.coreCmd = constants.CORE_CMD
         self.bsfCmd = constants.BS_FEATURE
-
-    def lsof_handles(self):
-        # TODO: Add some error checking around invalid PID
-        command_token = self.numFileHandlesCmd
-        # throw away the first two lines of lsof header output
-        return int(self._run_cmd(command_token)) - 1
-
-    def meminfo(self):
-        command_token = self.meminfoCmd
-        return self._run_cmd(command_token)
-
-    def system_load(self):
-        command_token = self.systemLoadCmd
-        return self._run_cmd(command_token)
-
-    def lsof(self):
-        command_token = self.lsofCmd + self.pid
-        return self._run_cmd(command_token)
-
-    def core(self):
-        command_token = self.coreCmd + self.pid
-        return self._run_cmd(command_token)
+        self.getPostgresCmd = constants.GET_POSTGRES
 
     def bs_feature(self):
         command_token = self.bsfCmd
