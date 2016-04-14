e44836ec936bd53aedfb6b8b13637896876ddf43
bohalloran <bohalloran@bitsighttech.com>
2016-04-14 09:08:13 -0400

    update bs-feature call to reliect current alias definition

--- lib/constants.py
+++ lib/constants.py
@@ -5,7 +5,7 @@ LOG_FILE_DATE_TIME_FORMAT = "%Y%m%dT%H%M%S"
 JENKINS_PROJECT_NAME = 'Build-Apt-Feature-Merge'
 
 # command line settings
-BS_FEATURE = 'sudo apt-get update;sudo apt-get install -y -t feature --only-upgrade bs-.*'
+BS_FEATURE = "sudo apt-get update;sudo apt-get install -y -t feature --only-upgrade '\''^bs-.*'\''"
 GET_POSTGRES = '. /etc/bs.vars ; sudo -u postgres python /usr/local/bitsight/postgres-wal-restore/get_postgres.py em101.prd1 LATEST --no-push'
 CHOWN_BITSIGHT = 'sudo chown -R bitsight.bitsight /usr/local/bitsight'
 CHEF_CLIENT = 'sudo chef-client'
